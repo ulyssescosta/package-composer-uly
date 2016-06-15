b9622326aceaca1987c9ef3f03dd46ca6e7c2c7d
Ulysses Costa <costa.ulysses@gmail.com>
2016-06-15 11:34:22 -0400

    Atualizando e corrigindo classe HelloPackage.php

--- src/Uly/HelloPackage.php
+++ src/Uly/HelloPackage.php
@@ -2,19 +2,13 @@
 /**
  * Description of HelloPackage
  *
- * @author Ulysses
+ * @author Ulysses Costa
  */
-
-<<<<<<< HEAD
+ 
 namespace Uly;
 
 class HelloPackage {
    public function getHello() {
       return "Olá, Ulysses! Seja Bem Vindo ao Curso de Composer com PHP!";
-=======
-class HelloPackage {
-   public function getHello() {
-      return "Olá, Ulysses!";
->>>>>>> 5967747c258e2f5d6df4407dbdbf3b427308b195
-   }
+      }
 }
